fcd3c919c30d487ec571febaaa1cd0da7e6c5572
Claus Damgaard <jinxxed@outlook.com>
2023-03-30 17:03:37 +0200

    Removed unnecessary using directives.

---
+++ Die.cs
@@ -0,0 +1,28 @@
+﻿namespace SLVP_Week5_DiesAndDiecups2
+{
+    internal class Die
+    {
+        // Private fields naming according to https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
+        private static Random _rand;
+        private int _value;
+
+        public Die()
+        {
+            _rand = new Random();
+            _value = _rand.Next(1, 7);
+        }
+
+        public void Roll()
+        {
+            _value = _rand.Next(1, 7);
+        }
+
+        public int GetValue()
+        {
+            return _value;
+        }
+
+    }
+
+
+}
---
+++ DieCup.cs
@@ -0,0 +1,31 @@
+﻿namespace SLVP_Week5_DiesAndDiecups2
+{
+    internal class DieCup
+    {
+        private Die d1;
+        private Die d2;
+
+        public DieCup()
+        {
+            d1 = new Die();
+            d2 = new Die();
+        }
+
+        public void Roll()
+        {
+            d1.Roll();
+            d2.Roll();
+        }
+
+        public int GetValue()
+        {
+            return d1.GetValue() + d2.GetValue();
+        }
+
+        public string ToString()
+        {
+            return d1.GetValue() + " + " + d2.GetValue() + " = " + GetValue();
+        }
+       
+    }
+}
